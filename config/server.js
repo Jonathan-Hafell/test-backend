51d7525a7c209e15c9cb3c53521f941ab62843de
Jonathan Hafell <Jonathan.hafell@gmail.com>
2021-03-24 13:45:10 +0100

    initialised project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e2410d743c47355797433281d9fee545'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '718c24e0-b769-44f4-b42c-e977bb502dba'
+};
